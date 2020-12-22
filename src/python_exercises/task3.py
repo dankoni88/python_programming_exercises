94089b3810ab45a48a3f61020775f5957e49335c
Danka Marjanovic <danka.marjanovic@softerrific.com>
2020-12-22 17:20:21 +0100

    added task 4

--- src/python_exercises/task3.py
+++ src/python_exercises/task3.py
@@ -1,9 +1,8 @@
 """
 With a given whole number n,
- write a program to generate a dictionary that contains (i, i x i)
-  such that is an integral number between 1 and n (both included).
-   and then the program should print the dictionary.
-   Suppose the following input is supplied to the program: 8
+write a program to generate a dictionary that contains (i, i x i)
+such that for a whole number between 1 and n (both included)
+the program should print the dictionary.
 """
 
 
---
+++ src/python_exercises/task4.py
@@ -0,0 +1,20 @@
+"""
+Write a program which accepts a sequence of comma-separated numbers
+from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program: 34,67,55,33,12,98
+"""
+
+
+def get_list_and_tuple_out_of_a_sequence(sequence):
+    list_of_values = [x for x in sequence.split(',')]
+    tuple_of_values = tuple(list_of_values)
+    print(list_of_values)
+    print(tuple_of_values)
+
+
+def main():
+    sequence= input("Enter a sequence of values: ")
+    get_list_and_tuple_out_of_a_sequence(sequence)
+
+if __name__ == '__main__':
+    main()
